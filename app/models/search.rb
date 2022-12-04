e765918de89c04c6894250243d7d9c4a868d1491
Div685 <divyeshpatel685@gmail.com>
2022-12-04 16:23:37 +0300

    Add search Controller
    - Updated Search controller to Save data on database.

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,11 @@
 class Search < ApplicationRecord
+  validates_presence_of :query
+  validates :query, length: { minimum: 3, maximum: 90 }
+
+  def searched?(search)
+    jarow = FuzzyStringMatch::JaroWinkler.create( :native )
+    similarity_percentage = jarow.getDistance(search, self.query)
+    return similarity_percentage > 0.8 ? true : false
+  end
+
 end
