332ba89658b7f69cd866ac282fb40369bc8a0568
Div685 <divyeshpatel685@gmail.com>
2022-12-04 16:20:35 +0300

    Add Realtime search functionality

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,4 +1,37 @@
 class ArticleController < ApplicationController
+  before_action :check_session, only: [:search]
+
   def index
+    @articles = Article.all
   end
+
+  def search
+    articles = Article.where('title LIKE ?', "%#{params[:query]}%")
+    render(partial: 'articles', locals: { articles: articles })
+    add_search_todb(params[:query], session[:user_id])
+  end
+
+  private
+
+  def add_search_todb(query, session)
+    return if query.nil? || query.length < 3
+
+    recent_search = Search.new(user_params)
+    recent_search.session_id = session
+    session_search = Search.where(session_id: session).last
+    if session_search.nil? || !session_search.searched?(query)
+      recent_search.save
+    elsif session_search.query.length < query.length
+      session_search.update(query: query)
+    end
+  end
+
+  def user_params
+    params.permit(:query)
+  end
+
+  def check_session
+    session[:user_id] = SecureRandom.urlsafe_base64(16) if session[:user_id].nil?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'search/index'
   root 'article#index'
+  resources 'article', only: [:index]
+  resources 'search', only: [:index]
+  post 'article/search', to: 'article#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
